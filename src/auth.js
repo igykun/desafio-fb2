e71298a600a31a58adc80fe0a4c4e2f80c5f15f9
Ignacio Jara <Igykun@gmail.com>
2024-11-28 17:08:22 -0300

    uploading develop

--- src/auth.js
+++ src/auth.js
@@ -8,16 +8,6 @@ import {
   signOut
 } from 'firebase/auth';
 
-// const firebaseConfig={
-//     apiKey: "AIzaSyBtRX2yVyj9yx_t4ukpDW3z_pgnW8qq_kQ",
-//   authDomain: "fir-auth-d83ed.firebaseapp.com",
-//   projectId: "fir-auth-d83ed",
-//   storageBucket: "fir-auth-d83ed.firebasestorage.app",
-//   messagingSenderId: "779974783700",
-//   appId: "1:779974783700:web:90ba4e14f9c2a619b88141"
-// }
-
-// const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 
 const logout = async () =>{
